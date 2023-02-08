4ebf796ad2eae67fd818b01c1d5dbfb077283d57
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2023-02-07 21:56:31 -0300

    v7

--- php_class/php_books/v7/PersonList.php
+++ php_class/php_books/v7/PersonList.php
@@ -4,23 +4,50 @@ require_once __DIR__ '/classes/Person.php';
 
 Class PersonList{
 
+    private $html;
+
     public function __construct(){
 
-
+        $this->html = file_get_contents('html/list.html');
 
     }
 
-    public funcrion load(){
+    public function delete($param){
+        try{
+            $id = (int) $param['id'];
+            Person::delete($id);
+        }catch(Exception $e){
+            print $e->getMessage();
+        }
+    }
+
+    public function load(){
 
         try{
             $rows = '';
             foreach(Person :: all() as $person){
                 $row = file_get_contents('html/row.html');
 
-                $this-> html = str_replace(['{id}', '{cep}', '{name}', '{address}', '{district}',
-                '{phone}'], [$this->data['id'], $this->data['cep'],
-                 $this->data['name'], $this->data['address'], $this->data['id']]);
+
+
+               $row = str_replace(['{id}', '{cep}', '{name}', '{address}', '{district}',
+                '{phone}'], 
+                [$person['id'],$person['name'],
+                $person['address'], $person['district'], $person['phone']],
+                $row
+            );
+            $rows .= $row;
             }
+
+            $this->html = str_replace(
+                '{rows}', $rows, $this->html
+            );
+        }catch(Exception $e){
+            print $e->getMessage();
         }
+    }
+
+    public function show(){
+        $this
     }
 }
