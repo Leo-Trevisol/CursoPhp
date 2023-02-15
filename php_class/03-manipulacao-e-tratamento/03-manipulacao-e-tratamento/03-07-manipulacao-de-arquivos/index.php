f2a99de081f25365a196fdfd2dab07f471e724d8
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2023-02-15 19:35:51 -0300

    manipulacao de arquivos

--- php_class/03-manipulacao-e-tratamento/03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
+++ php_class/03-manipulacao-e-tratamento/03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
@@ -2,6 +2,20 @@
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.07 - Manipulação de arquivos");
 
+$file = __DIR__ . '/arquivo.txt';
+if(file_exists($file) && (is_file($file))){
+    echo "existe";
+}else{
+    echo "nao existe";
+}
+
+if(!file_exists($file) || !is_file($file)){
+    $fileopen = fopen($file, "w");
+
+}else{
+    var_dump(file($file),pathinfo($file));
+}
+
 /*
  * [ verificação de arquivos ] file_exists | is_file | is_dir
  */
