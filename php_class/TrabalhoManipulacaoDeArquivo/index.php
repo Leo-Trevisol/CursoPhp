ec730ec36bd4196c2d0084d6478baec39dd3f098
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2023-02-16 19:46:40 -0300

    funcionando

--- php_class/TrabalhoManipulacaoDeArquivo/index.php
+++ php_class/TrabalhoManipulacaoDeArquivo/index.php
@@ -1,21 +1,81 @@
 <?php
+require __DIR__ . '/fullstackphp/fsphp.php';
+fullStackPHPClassName("03.07 - Manipulação de arquivos");
 
-$getcontents = __DIR__ . "/listaTelefonica.txt";
+$file = __DIR__ . '/listaTelefonica.txt';
 
-// if(file_exists($getcontents) && is_file($getcontents)){
-//       echo file_get_contents($getcontents);
-// }else{
 
-//     $data = "<article><h1> leozin bacana (54) 98408-1674 </h1> </article>";
-//  file_put_contents($getcontents, $data);
 
-// }
-
-$read = fopen($getcontents, 'r');
-
-while(!feof($read)){
-    echo "<article> <h2> " . fgets($read) . " </h2> </article>";
+if(file_exists($file) && (is_file($file))){
+    
+}else{
+    echo "nao existe";
 }
+
+
+
+
+/*
+ * [ verificação de arquivos ] file_exists | is_file | is_dir
+ */
+fullStackPHPClassSession("verificação", __LINE__);
+
+
+/*
+ * [ leitura e gravação ] fopen | fwrite | fclose | file
+ */
+fullStackPHPClassSession("leitura e gravação", __LINE__);
+
+
+/*
+ * [ get, put content ] file_get_contents | file_put_contents
+ */
+fullStackPHPClassSession("get, put content", __LINE__);
 
 ?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de telefones</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+
+    <h1>Cadastro de telefones</h1>
+
+    <form  action=""  method="post">
+
+        <label for="name">Nome:</label>
+        <input type="text" name="name" id="name">
+        <label for="number">Numero:</label>
+        <input type="number" name="number" id="number">
+        <input type="submit" name="bt" id="bt">
+
+    </form>
+
+    <?php
+
+    $read = fopen($file, 'r');
+    while (!feof($read)){
+            echo "<article> " , "<span></span> " , "<b> </b>", "</article>";
+        }
+
+        $name = $_POST['name'];
+$number = $_POST['number'];
+
+if(!file_exists($file) || !is_file($file)){
+    $fileopen = fopen($file, "w");
+    fwrite($fileopen, $name . PHP_EOL . $number);
+    fclose($fileopen);
+
+}
+    
+
+    ?>
+    
+</body>
+</html>
--- php_class/TrabalhoManipulacaoDeArquivo/teste.php
+++ php_class/TrabalhoManipulacaoDeArquivo/teste.php
@@ -1,12 +1,41 @@
 <?php
 
+$lista = __DIR__ . "/listaTelefonica.txt";
 
- 
-if (isset($_POST['name']) &&isset($_POST['number']) ) {
-   $name = $_POST['name'];
-   $number = $_POST['number'];
- 
-   $arquivo = fopen('listaTelefonica.txt', 'w');
-   fwrite($arquivo, $name . PHP_EOL . $number);
-   fclose($arquivo);
+if(file_exists($lista) && (is_file($lista))){
+   if (isset($_POST['name']) &&isset($_POST['number']) ) {
+       file_put_contents($lista, "{$_REQUEST['name']} - {$_REQUEST['number']}" .PHP_EOL);
+   }
+}else{
+  
+      file_put_contents($lista, "{$_REQUEST['name']} - {$_REQUEST['number']}" .PHP_EOL, FILE_APPEND);
 }
+
+?>
+ 
+ <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de telefones</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+
+    <h1>Cadastro de telefones</h1>
+
+    <form  action=""  method="post">
+
+        <label for="name">Nome:</label>
+        <input type="text" name="name" id="name">
+        <label for="number">Numero:</label>
+        <input type="number" name="number" id="number">
+        <input type="submit" name="bt" id="bt">
+
+    </form>
+
+    
+</body>
+</html>
