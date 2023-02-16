e694e63dd90449f264eb48ebb37b3ba5cdd5d8f5
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2023-02-16 00:33:04 -0300

    salvando

--- php_class/TrabalhoManipulacaoDeArquivo/index.php
+++ php_class/TrabalhoManipulacaoDeArquivo/index.php
@@ -1,18 +1,20 @@
 <?php
 
-
-
 $getcontents = __DIR__ . "/listaTelefonica.txt";
 
+// if(file_exists($getcontents) && is_file($getcontents)){
+//       echo file_get_contents($getcontents);
+// }else{
 
+//     $data = "<article><h1> leozin bacana (54) 98408-1674 </h1> </article>";
+//  file_put_contents($getcontents, $data);
 
-if(file_exists($getcontents) && is_file($getcontents)){
-      echo file_get_contents($getcontents);
-}else{
+// }
 
-    $data = "<article><h1>$name</h1> . <h2> $number </h2> </article>";
- file_put_contents($getcontents, $data);
+$read = fopen($getcontents, 'r');
 
+while(!feof($read)){
+    echo "<article> <h2> " . fgets($read) . " </h2> </article>";
 }
 
 ?>
